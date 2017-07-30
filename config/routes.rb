c490677bb905802ae33d9add53a9da0806a88bd7
Geoff Hull <geoffhull03@gmail.com>
2017-07-30 16:45:54 -0700

    Finished Seeding

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
   get 'items/create'
 
+  get 'users/id'
+
   root 'users#show'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+10.times do
+    User.create!(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(6),
+    )
+end
+users = User.all
+
+100.times do
+    Item.create!(
+      user: users.sample,
+      name: Faker::FamilyGuy.quote
+    )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
