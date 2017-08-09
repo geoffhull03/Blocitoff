dc113492f0b83394593aee61a1962f5d7c73a323
Geoff Hull <geoffhull03@gmail.com>
2017-08-08 17:37:19 -0700

    Refactored Item Controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,27 @@
 class ItemsController < ApplicationController
 
   def create
-    @user = User.find(current_user.id)
-    @item = Item.new
-    @item.name = params[:item][:name]
-    @item.user = current_user
+    @item = current_user.items.build(name: params[:item][:name])
 
     if @item.save
       flash[:success] = "Item was saved."
-      redirect_to user_path(@user.id)
+      redirect_to user_path(current_user)
     else
       flash[:alert] = "There was an error saving the item. Please try again."
-      redirect_to user_path(@user.id)
+      redirect_to user_path(current_user)
     end
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @user = User.find(current_user.id)
+    @item = current_user.items.find(params[:id])
 
     if @item.destroy
       flash[:notice] = "You've completed an item!"
-      redirect_to user_path(@user.id)
+      redirect_to user_path(current_user)
     else
+
       flash.now[:alert] = "There was a problem checking off your task."
-      redirect_to user_path(@user.id)
+      redirect_to user_path(current_user)
     end
   end
 end
